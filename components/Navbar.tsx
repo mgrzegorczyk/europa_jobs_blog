7c86b6cd4db693f75bdcf931ed3bfadb1a44fdc5
mgrzegor <m-grzegorczyk@o2.pl>
2024-07-09 23:01:27 +0200

    navbar - active link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+    const pathname = usePathname();
+
     return (
         <nav className="bg-gray-800 p-4">
             <ul className="flex space-x-4 justify-center">
                 <li>
-                    <Link href="/" className="text-white hover:text-gray-300">
+                    <Link href="/" className={`text-white hover:text-gray-300 ${pathname === '/' ? 'text-orange-500' : ''}`}>
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link href="/blog" className="text-white hover:text-gray-300">
-                       Blog
+                    <Link href="/blog" className={`text-white hover:text-gray-300 ${pathname === '/blog' ? 'text-orange-500' : ''}`}>
+                        Blog
                     </Link>
                 </li>
             </ul>
