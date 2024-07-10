cbfbd6c8f872494f33af1c6cf540f201b1c13e6c
mgrzegor <m-grzegorczyk@o2.pl>
2024-07-10 02:40:08 +0200

    RecommendedPostCard - image size fixe

--- components/RecommendedPostCard.tsx
+++ components/RecommendedPostCard.tsx
@@ -13,16 +13,17 @@ interface RecommendedPostCardProps {
     post: RecommendedPost;
 }
 
-const RecommendedPostCard = ({ post } : RecommendedPostCardProps) => {
+const RecommendedPostCard = ({ post }: RecommendedPostCardProps) => {
     return (
         <a href={`/posts/${post.key}`} className="block border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-            <Image src={post.mediaUrl}
-                   alt={post.title}
-                   className="w-full h-32 object-cover"
-                   layout="responsive"
-                   width={16}
-                   height={9}
-            />
+            <div className="relative w-full h-24">
+                <Image
+                    src={post.mediaUrl}
+                    alt={post.title}
+                    className="object-cover"
+                    layout="fill"
+                />
+            </div>
             <div className="p-2">
                 <h3 className="text-sm font-semibold">{post.title}</h3>
                 <p className="text-xs text-gray-500">{new Date(post.addedAt).toLocaleDateString()}</p>
