86f84c6ec19363e8c499dc21e17a6b850ef0339b
mgrzegor <m-grzegorczyk@o2.pl>
2024-07-10 02:22:40 +0200

    navbar - text styles fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
 
     return (
         <nav className="bg-gray-800 p-4">
-            <ul className="flex space-x-4 justify-center">
+            <ul className="text-white flex space-x-4 justify-center">
                 <li>
-                    <Link href="/" className={`text-white hover:text-gray-300 ${pathname === '/' ? 'text-orange-500' : ''}`}>
+                    <Link href="/" className={`hover:text-orange-300 ${pathname === '/' ? 'text-orange-500' : ''}`}>
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link href="/blog" className={`text-white hover:text-gray-300 ${pathname === '/blog' ? 'text-orange-500' : ''}`}>
+                    <Link href="/blog" className={`hover:text-orange-300 ${pathname === '/blog' ? 'text-orange-500' : ''}`}>
                         Blog
                     </Link>
                 </li>
